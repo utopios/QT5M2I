3f64bb3e99efb01f706d177cdf072a0c8d015d5d
ihab <ihab@utopios.net>
2023-05-15 12:30:54 +0200

    debut de correction de l'exercice 1

--- CLion-demo-app/main.cpp
+++ CLion-demo-app/main.cpp
@@ -2,10 +2,23 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QLineEdit>
+#include <QSpinBox>
+
+static QWidget* createLineWidget(QString content, int xPosition, QWidget* parent = nullptr) {
+    QWidget* widget = new QWidget(parent);
+    QLabel* label = new QLabel(content, widget);
+    label->resize(100, 30);
+    QLineEdit* nameLineEdit = new QLineEdit(widget);
+    nameLineEdit->resize(100, 30);
+    nameLineEdit->move(xPosition, 0);
+    widget->move(0, xPosition);
+    return widget;
+}
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QWidget widget;
+    //QWidget widget;
     // Création des premiers Widgets
 //    //widget de type label
 //    QLabel label("Content of label", &widget);
@@ -16,14 +29,28 @@ int main(int argc, char *argv[]) {
 
 ////    button.show();
 
-    QLabel label("Name", &widget);
-    label.resize(100, 30);
-    QLineEdit nameLineEdit(&widget);
-    nameLineEdit.resize(100, 30);
-    nameLineEdit.move(120, 0);
-    QPushButton button("Ok", &widget);
-    button.resize(200,30);
-    button.move(0, 40);
-    widget.show();
+//    QLabel label("Name", &widget);
+//    label.resize(100, 30);
+//    QLineEdit nameLineEdit(&widget);
+//    nameLineEdit.resize(100, 30);
+//    nameLineEdit.move(120, 0);
+//    QPushButton button("Ok", &widget);
+//    button.resize(200,30);
+//    button.move(0, 40);
+//    widget.show();
+
+
+//Correction EX1
+    QWidget mainWidget;
+    int position = 0;
+    QWidget* firstNameWidget = createLineWidget("First Name", position, &mainWidget);
+    //firstNameWidget->setParent(&mainWidget);
+    position += 30;
+    QWidget* lastNameWidget = createLineWidget("Last Name", position, &mainWidget);
+    //lastNameWidget->setParent(&mainWidget);
+    position += 60;
+    QWidget* phoneWidget = createLineWidget("Phone ", position, &mainWidget);
+    //phoneWidget->setParent(&mainWidget);
+    mainWidget.show();
     return QApplication::exec();
 }
