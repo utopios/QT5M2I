ba3180d3101790758eadeba002eb1ab0c9dbfdad
ihab <ihab@utopios.net>
2023-05-22 14:55:34 +0200

    suite cours mvc

--- CoursMVC/CoursMVC/mainwindow.cpp
+++ CoursMVC/CoursMVC/mainwindow.cpp
@@ -6,38 +6,57 @@
 #include <QPushButton>
 #include <QInputDialog>
 #include <QLineEdit>
+#include <QSqlTableModel>
+#include <QDir>
+#include <QSqlQuery>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //Information base de données sqlite
+    QString dataFolderPath = QCoreApplication::applicationDirPath() + "/data";
+    QDir().mkpath(dataFolderPath);
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(dataFolderPath + "/mydatabase.db");
+    //Création d'une table exemple
+    QSqlQuery query;
+    query.exec("CREATE TABLE items(name TEXT)");
+
+    //Widget pour demo
     QWidget* widget = new QWidget(this);
     QVBoxLayout* boxLayout = new QVBoxLayout(widget);
 
     //Bouton pour ajouter
-    QPushButton* button = new QPushButton("ajouter", this);
+    QPushButton* button = new QPushButton("ajouter", widget);
 
     //View
-    QListView* listView = new QListView();
+    QLineEdit* line = new QLineEdit(widget);
+    QListView* listView = new QListView(widget);
 
     //Model
-    QStringListModel* model = new QStringListModel();
+//    QStringListModel* model = new QStringListModel();
+//    listView->setModel(model);
+//    model->insertRow(model->rowCount());
+//    model->setData(model->index(model->rowCount() -1 ), "toto");
+
+//    //Ouvrir une boite de dialogue pour modifier le model
+//    QObject::connect(button, &QPushButton::clicked, [&]() {
+//        model->insertRow(model->rowCount());
+//        model->setData(model->index(model->rowCount() -1 ), line->text());
+//    });
+
+    //Model QSqlTableModel
+    QSqlTableModel* model = new QSqlTableModel(listView,db);
+    model->setTable("items");
+    model->select();
+    //Bind du mode lavec la vue
     listView->setModel(model);
-    model->insertRow(model->rowCount());
-    model->setData(model->index(model->rowCount() -1 ), "toto");
-
-    //Ouvrir une boite de dialogue pour modifier le model
-    QObject::connect(button, &QPushButton::clicked, [&]() {
-        bool ok;
-        QString text = QInputDialog::getText(this, "Saisir un element", "Valider", QLineEdit::Normal, "", &ok);
-        if(ok) {
-            model->insertRow(model->rowCount());
-            model->setData(model->index(model->rowCount() -1 ), text);
-        }
-    });
-
 
     boxLayout->addWidget(listView);
+    boxLayout->addWidget(line);
+    boxLayout->addWidget(button);
     setCentralWidget(widget);
 
 
